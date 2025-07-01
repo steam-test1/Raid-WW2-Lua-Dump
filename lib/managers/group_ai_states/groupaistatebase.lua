f6b1089cc9bc059cc5b02328f724904d22a53aeb
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2025-07-01 11:40:05 +0100

    21.5

--- lib/managers/group_ai_states/groupaistatebase.lua
+++ lib/managers/group_ai_states/groupaistatebase.lua
@@ -3124,13 +3124,7 @@ function GroupAIStateBase:sync_assault_mode(enabled)
 end
 
 function GroupAIStateBase:_lightfx_assault_update()
-	if _G.IS_PC and managers.network and managers.network.account:has_alienware() then
-		if self._assault_mode then
-			LightFX:set_lamps(255, 0, 0, 255)
-		else
-			LightFX:set_lamps(0, 255, 0, 255)
-		end
-	end
+	return
 end
 
 function GroupAIStateBase:set_fake_assault_mode(enabled)
